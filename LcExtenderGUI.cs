d9be0d74cb58db2e792ae12877b69c77b05bef82
henry <henry@HENRYPC>
2025-07-10 17:49:04 +0200

    Add project files.

---
+++ LcExtenderGUI.cs
@@ -0,0 +1,83 @@
+﻿using Cysharp.Threading.Tasks;
+using Il2CppSystem.Threading;
+using SiNiSistar2;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SiNiSistar2LcExtender
+{
+    internal class LcExtenderGUI : MonoBehaviour
+    {
+        public static List<int> DramaTasks = new List<int>();
+        public static bool DramaCleanRequested = false;
+
+        public bool IsVisible = true;
+        public void Update()
+        {
+            if (Keyboard.current.altKey.wasPressedThisFrame)
+            {
+                IsVisible = !IsVisible;
+            }
+            if (Keyboard.current.digit1Key.wasPressedThisFrame && IsVisible)
+            {
+                Plugin.GenerateTranslationTemplate();
+            }
+            if (Keyboard.current.digit2Key.wasPressedThisFrame && IsVisible)
+            {
+                Plugin.Instance.HotReloadLanguageMods();
+            }
+            if (Keyboard.current.digit3Key.wasPressedThisFrame && IsVisible)
+            {
+                Plugin.Instance.SwitchLanguage();
+            }
+
+            // Queue up drama files that are being loaded (indicated by initial null value)
+            foreach (var kvp in DramaLoader.DramaFileDictionary)
+            {
+                if (kvp.Value == null && !DramaTasks.Contains((int)kvp.Key))
+                {
+                    DramaTasks.Add((int)kvp.Key);
+                }
+            }
+
+            // Process the Queued up tasks, if they are done loading they shouldnt be null anymore
+            for (int i = DramaTasks.Count - 1; i >= 0; i--)
+            {
+                if (DramaLoader.DramaFileDictionary.TryGetValue((DramaID)DramaTasks[i], out var dramaFile) && dramaFile != null)
+                {
+                    if (Plugin.Instance.CurrentLanguage != "" && !LcExtenderGUI.DramaCleanRequested)
+                    {
+                        Plugin.Instance.ApplyTranslation(dramaFile);
+                    }
+                    DramaTasks.RemoveAt(i);
+                }
+            }
+            if (DramaCleanRequested && DramaTasks.Count == 0)
+            {
+                Plugin.FinishExporting();
+                DramaCleanRequested = false;
+            }
+        }
+
+        public static void TriggerAllDramaTasksClean()
+        {
+            if (DramaCleanRequested) return; // Avoid multiple triggers
+            DramaCleanRequested = true;
+            DramaLoader.ClearLoader();
+
+            // foreach drama id load and forget
+            for (int i = 1; i < typeof(DramaID).GetEnumValues().Length; i++)
+            {
+                DramaLoader.LoadDramaFile((DramaID)i, CancellationToken.None).Forget();
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (!IsVisible) return;
+            GUI.Label(new Rect(10, 400, 700, 20), $"Alt: Hide | 1: Generate Translation Template | 2: Reload Languages | 3: Switch Language | Current Language: {(Plugin.Instance.CurrentLanguage == "" ? "None" : Plugin.Instance.CurrentLanguage)}");
+
+        }
+    }
+}
---
+++ LocalizeManagerPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using SiNiSistar2.Lc;
+using SiNiSistar2.Manager;
+using UnityEngine;
+
+namespace SiNiSistar2LcExtender
+{
+    [HarmonyPatch(typeof(LocalizeManager))]
+    internal class LocalizeManagerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("GetLcText")]
+        public static void GetLcTextPostfix(LocalizeID id, ref string __result)
+        {
+            if (Plugin.Instance.DiscoveredLanguageMods.TryGetValue(Plugin.Instance.CurrentLanguage, out var modConfig) && modConfig.LocalizationTable != null && modConfig.LocalizationTable.ContainsKey(id.ToString()))
+            {
+                __result = modConfig.LocalizationTable[id.ToString()].Replace("[[PlayerName]]", ManagerList.PlayerStatus.PlayerName);
+                Plugin.Instance.Log.LogInfo($"LocalizeID: {id}, Result: {__result}");
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("GetLcTextChoice")]
+        public static void GetLcTextChoicePostfix(ChoiceLocalizeID id, ref string __result)
+        {
+            if (Plugin.Instance.DiscoveredLanguageMods.TryGetValue(Plugin.Instance.CurrentLanguage, out var modConfig) && modConfig.LocalizationTableChoice != null && modConfig.LocalizationTableChoice.ContainsKey(id.ToString()))
+            {
+                __result = modConfig.LocalizationTableChoice[id.ToString()].Replace("[[PlayerName]]", ManagerList.PlayerStatus.PlayerName);
+                Plugin.Instance.Log.LogInfo($"ChoiceLocalizeID: {id}, Result: {__result}");
+            }
+        }
+    }
+}
